294477adfd7abc4e18e7766f64cd0d99251802b5
Juraj Lodňan <ueden@centrum.cz>
2026-03-03 11:06:51 +0100

    JWT, registration, validation, authentication, email validation

---
+++ book-network/src/main/java/com/ueden/book/auth/AuthenticationService.java
@@ -0,0 +1,134 @@
+package com.ueden.book.auth;
+
+import com.ueden.book.email.EmailService;
+import com.ueden.book.email.EmailTemplateName;
+import com.ueden.book.role.Role;
+import com.ueden.book.role.RoleRepository;
+import com.ueden.book.security.JwtService;
+import com.ueden.book.user.Token;
+import com.ueden.book.user.TokenRepository;
+import com.ueden.book.user.User;
+import com.ueden.book.user.UserRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public void register(RegistrationRequest registrationRequest) throws MessagingException {
+        Role userRole = roleRepository.findByName("USER")
+                // TODO better ex handling
+                .orElseThrow(() -> new IllegalStateException("Role USER not initialized"));
+
+        User user = User.builder()
+                .firstName(registrationRequest.getFirstname())
+                .lastName(registrationRequest.getLastname())
+                .email(registrationRequest.getEmail())
+                .password(passwordEncoder.encode(registrationRequest.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+        emailService.sendEmail(
+                user.getEmail(),
+                user.getFullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account activation"
+
+        );
+
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        String generatedToken = generateActivationToken(6);
+        Token token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationToken(int length) {
+        String characters = "0123456789";
+        StringBuilder tokenBuuilder = new StringBuilder();
+        SecureRandom random = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            tokenBuuilder.append(characters.charAt(randomIndex));
+        }
+        return tokenBuuilder.toString();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        User  user = (User)auth.getPrincipal();
+        var claims = new HashMap<String, Object>();
+        claims.put("fullName", user.getFullName());
+
+        var jwtToken = jwtService.generateToken(claims, user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    // @Transactional
+    public void activateAccount(String token) throws MessagingException {
+        Token savedToken = tokenRepository.findByToken(token)
+                .orElseThrow(() -> new RuntimeException("Token not valid"));
+        if(LocalDateTime.now().isAfter(savedToken.getExpiresAt())) {
+            sendValidationEmail(savedToken.getUser());
+            throw new RuntimeException("Activation token expired. New token has been sent to email");
+        }
+
+        User user = userRepository.findById(savedToken.getUser().getId())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        user.setEnabled(true);
+        userRepository.save(user);
+        savedToken.setValidatedAt(LocalDateTime.now());
+        tokenRepository.save(savedToken);
+
+    }
+}
---
+++ book-network/src/main/java/com/ueden/book/auth/RegistrationRequest.java
@@ -0,0 +1,36 @@
+package com.ueden.book.auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationRequest {
+
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String firstname;
+
+    @NotEmpty(message = "Lastname is mandatory")
+    @NotBlank(message = "Lastname is mandatory")
+    private String lastname;
+
+    @Email(message = "Email is not formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be at least 8 characters long")
+    private String password;
+}
