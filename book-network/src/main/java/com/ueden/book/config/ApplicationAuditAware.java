4707d9409ab892e4f135dfe8597c8ef3c12c6cf8
Juraj Lodňan <ueden@centrum.cz>
2026-03-09 13:24:22 +0100

    Add AuditorAware to support BaseEntity.createdBy and lastModifiedBy methods

---
+++ book-network/src/main/java/com/ueden/book/config/ApplicationAuditAware.java
@@ -0,0 +1,26 @@
+package com.ueden.book.config;
+
+import com.ueden.book.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware  implements AuditorAware<Integer> {
+
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null
+                || !authentication.isAuthenticated()
+                || authentication instanceof AnonymousAuthenticationToken){
+            return Optional.empty();
+        }
+
+        User principal = (User) authentication.getPrincipal();
+
+        return Optional.ofNullable(principal.getId());
+    }
+}
