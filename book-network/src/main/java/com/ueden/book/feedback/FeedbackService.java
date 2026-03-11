c6ac1e7364b2312bab42bbe16482b1cc9bac7350
Juraj Lodňan <ueden@centrum.cz>
2026-03-11 10:05:16 +0100

    Add feedback save and findAllFeedbacksByBook methods

---
+++ book-network/src/main/java/com/ueden/book/feedback/FeedbackService.java
@@ -0,0 +1,59 @@
+package com.ueden.book.feedback;
+
+import com.ueden.book.book.Book;
+import com.ueden.book.book.BookRepository;
+import com.ueden.book.common.PageResponse;
+import com.ueden.book.exception.OperationNotPermittedException;
+import com.ueden.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+    private final FeedbackRepository feedbackRepository;
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:" + request.bookId()));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for and archived or not shareable book");
+        }
+        User user = (User) connectedUser.getPrincipal();
+        if (Objects.equals(book.getOwner().getId(), user.getId())) {
+            throw new OperationNotPermittedException("You cannot give feedback to your own book");
+        }
+
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int pageNumber, int pageSize, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(feedback -> feedbackMapper.toFeedbackResponse(feedback, user.getId()))
+                .toList();
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
+    }
+}
